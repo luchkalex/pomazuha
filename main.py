fd584dd17413fd4d16250ffc946f80dca6b7a3f9
GavnoHacker <61355491+GavnHacker@users.noreply.github.com>
2023-03-14 11:01:53 +0200

    123

--- main.py
+++ main.py
@@ -1,19 +1,18 @@
-import pygame, sys
-
-
-WIDTH, HEIGHT = 1920, 1080
-TEXT_COL = (255, 255, 255)
-resume_btn = pygame.image.load("assets/menu_bg/start_btn.png").convert_alpha()
-exit_btn = pygame.image.load("assets/menu_bg/exit_btn.png").convert_alpha()
-
-
+import pygame, sys, os
 pygame.init()
+
+
+WINDOW_SCALE = (1280, 720)
+TEXT_COL = (255, 255, 255)
+
 font = pygame.font.SysFont("arialblack", 40)
 
-SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
+SCREEN = pygame.display.set_mode(WINDOW_SCALE)
 pygame.display.set_caption("pomazuha")
 
-BG = pygame.image.load("assets/menu_bg/main_menu.jpg")
+BG = pygame.image.load("assets/menu_bg/menu1.jpg")
+BG = pygame.transform.scale(BG, WINDOW_SCALE)
+
 pygame.mixer.music.load("sounds/music/main_menu.mp3")
 pygame.mixer.music.play()
 
@@ -25,14 +24,21 @@ def draw_text(text, font, text_col, x, y):
 
 run = True
 while run:
-		SCREEN.fill([255, 255, 255])
-		SCREEN.blit(BG, (0, 0))
+		for event in pygame.event.get():
+				if event.type == pygame.QUIT:
+					run = False
 		
 
+		SCREEN.blit(BG, (0, 0))
 		draw_text("Roma HUY", font, TEXT_COL, 50, 50)
 
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
-				run = False
+		
+		
+
 		pygame.display.update()
+	
+
+
+		
+		
 pygame.quit()
