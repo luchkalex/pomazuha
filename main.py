fbbb9ac6934c507a80dc3d3fb4eff3fcdf4ad709
GavnoHacker <61355491+GavnHacker@users.noreply.github.com>
2023-03-14 09:48:11 +0200

    huy

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pygame, sys
 
 
-WIDTH, HEIGHT = 1280, 720
+WIDTH, HEIGHT = 1920, 1080
 TEXT_COL = (255, 255, 255)
 
 pygame.init()
@@ -10,7 +10,10 @@ font = pygame.font.SysFont("arialblack", 40)
 SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("pomazuha")
 
-BG = pygame.image.load("assets\menu_bg\main_menu.jpg")
+BG = pygame.image.load("assets/menu_bg/main_menu.jpg")
+pygame.mixer.music.load("sounds/music/main_menu.mp3")
+pygame.mixer.music.play()
+
 
 def draw_text(text, font, text_col, x, y):
      img = font.render(text, True, text_col)
@@ -18,7 +21,9 @@ def draw_text(text, font, text_col, x, y):
 
 run = True
 while run:
-		SCREEN.fill((52, 78, 92))
+		SCREEN.fill([255, 255, 255])
+		SCREEN.blit(BG, (0, 0))
+		
 
 		draw_text("Roma HUY", font, TEXT_COL, 50, 50)
 
