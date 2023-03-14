4055eb90cacf4e4e1cba6f9838307dc6a097acd0
GavnoHacker <61355491+GavnHacker@users.noreply.github.com>
2023-03-14 11:01:13 +0200

    начала для меню положено

---
+++ button.py
@@ -0,0 +1,16 @@
+class Button():
+    def __init__(self, x, y, width, height, buttonText='Button', onclickFunction=None, onePress=False):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.onclickFunction = onclickFunction
+        self.onePress = onePress
+        self.alreadyPressed = False
+
+        self.fillColors = {
+            'normal': '#ffffff',
+            'hover': '#666666',
+            'pressed': '#333333',
+        }
+        
